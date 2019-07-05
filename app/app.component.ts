73fa41266adc52a96190ec9c10b569eb89a4d52e
znimed <znimed@gmail.com>
2019-07-05 12:47:02 +0000

    Add firebase to project

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-model';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyAoDQnyhe53BK2A_x1ZNmMts80qDH9A3GI",
+      authDomain: "bookcatalog-1473e.firebaseapp.com",
+      databaseURL: "https://bookcatalog-1473e.firebaseio.com",
+      projectId: "bookcatalog-1473e",
+      storageBucket: "",
+      messagingSenderId: "596588004159",
+      appId: "1:596588004159:web:4cab6e426b225772"'
+    };
+    firebase.initializeApp(config);
+  }
 }
